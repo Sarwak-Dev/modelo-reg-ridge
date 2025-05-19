1bd9e488f0c89ec8366b9ae71ee60cb89a02dddb
Sarwak <sarwakpro@gmail.com>
2025-05-19 19:22:54 -0400

    #1

---
+++ plot.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_real_estimates(y_real, y_estimate):
+    resid = y_real- y_estimate
+
+    
+    
+
---
+++ tst.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------
+# Test of the best regression model
+#-------------------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+from utility import *
+   
+#---------------------------------------------------------------------
+def main():            
+    beta = load_data_csv('coefts.csv')	   
+    
+
+if __name__ == '__main__':   
+	 main()
+
